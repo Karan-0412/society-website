bf95f341c80abbd904d2af3137f9f695083042a2
Jasskaran Singh Shangari <jasskaransingh0007@gmail.com>
2025-09-10 18:19:37 +0530

    Remove Backend directory

--- Backend_karan/src/routes/message.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { sendMessage } from "../controllers/message.controller.js";
-
-const router = Router();
-
-router.route("/").post(sendMessage);
-
-export default router;
--- Backend_karan/src/routes/registeration.route.js
+++
@@ -1,8 +0,0 @@
-import { Router } from "express";
-import { registerTeam } from "../controllers/registeration.controller.js";
-
-const router = Router();
-
-router.post("/", registerTeam);
-
-export default router;
